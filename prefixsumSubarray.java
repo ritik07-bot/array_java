dc79d3cc46603f86eea46d4022f861581dc76941
Ritik Roushan <ritikroushan777@gmail.com>
2026-03-21 23:50:02 +0530

    max sum of subarray by prefix sum array

---
+++ prefixsumSubarray.java
@@ -0,0 +1,28 @@
+public class prefixsumSubarray {
+    public static void prefix_sum(int arr[]){
+        int prefixSum[]= new int[arr.length];
+         
+        prefixSum[0]=arr[0];
+        //prefix sum
+        for(int i=1;i<arr.length;i++){
+             prefixSum[i]=prefixSum[i-1]+arr[i];
+        }
+        
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            currsum=0;
+            for(int j=i;j<arr.length;j++){
+               currsum=i==0?prefixSum[j]:prefixSum[j]-prefixSum[i-1];
+               if(currsum>maxsum){
+                maxsum=currsum;
+               }
+            }
+        }System.out.println("maxsum is: "+maxsum);
+    }
+    public static void main(String args[]){
+       int arr[]={-5,-1,3,6,-5};
+       prefix_sum(arr);
+    }
+}
