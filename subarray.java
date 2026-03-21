e26654e46b9b268cf338572343a4c34a8f66c229
Ritik Roushan <ritikroushan777@gmail.com>
2026-03-21 19:38:15 +0530

    update in subarray in array

--- subarray.java
+++ subarray.java
@@ -1,17 +1,30 @@
 public class subarray {
     public static void subArray(int arr[]){
         int ts=0;
+        int minSum=Integer.MAX_VALUE;
+        int maxSum=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
             int start=i;
             for(int j=i;j<arr.length;j++){
                 int end=j;
+                int sum=0;
                 for(int k=start;k<=end;k++){
                     System.out.print(arr[k]+" ");
+                    sum+=arr[k];
                 }
                 ts++;
+                if(minSum>sum){
+                    minSum=sum;
+                }
+                if(maxSum<sum){
+                    maxSum=sum;
+                }
+                System.out.println(" sum is:"+sum);
                 System.out.println();
             }System.out.println();
         }System.out.println("total subarray :"+ts);
+        System.out.println("maxSum is: "+maxSum);
+        System.out.println("minSum is: "+minSum);
     }
     public static void main(String args[]){
         int arr[]={2,4,6,8,10};
